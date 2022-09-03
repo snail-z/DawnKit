6fbba2368f765e183b78cb039554ed76d56b09fb
张浩 <40460770@qq.com>
2022-09-03 08:42:42 +0800

    feat: Update

--- Example/AnswerExtensions/ViewController.swift
+++ Example/AnswerExtensions/ViewController.swift
@@ -14,8 +14,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        view.pk.showToast(message: "哈哈")
     }
 
     override func didReceiveMemoryWarning() {
